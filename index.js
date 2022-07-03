9713e9c59e0eecfa0fd180187246ba80cc271b7e
Kyle Vermillion <kvermillion23@gmail.com>
2022-07-02 20:25:33 -0400

    commit tree 2a22d2d8d444c6456b77662d2604f19fa08df321

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+const employee = {name: 'Kyle', streetaddress:'111 fart lane'};
+
+function updateEmployeeWithKeyAndValue(employee,key,value) {
+    return {...employee, [key]:value};
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value) {
+    employee[key] = value
+    return employee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key,value) {
+    delete employee[key]
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee,key,value) {
+    return {...employee,[key]:value};
+}
+
